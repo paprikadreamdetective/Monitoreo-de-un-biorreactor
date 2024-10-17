70f2316d005a1eea9154585558612712466d3b45
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-10-17 02:01:12 -0600

    clean code  in main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,3 @@
-#include <UnorderedMap.h>
-/*
-int i = 0;
-int j = 10;
-*/
-
 void setup() {
   Serial.begin(9600);
 }
@@ -14,14 +8,5 @@ void loop() {
   Serial.print(mq135_analog_value);
   Serial.print(",");
   Serial.println(mq3_analog_value);
-  /*if ( i >= 20 && j >= 30) {
-    i = 0;
-    j = 10;
-  }*/
-  /*Serial.print(i);
-  Serial.print(",");
-  Serial.println(j);
-  i++;
-  j++;*/
   delay(3000);
 }
